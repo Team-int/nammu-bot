6ce6d12bcf848d9e80202ba2caff0bccd480f2a6
lukekeum <lukekeum11@gmail.com>
2021-06-06 21:19:57 +0900

    feat(server): Register fastify-cookie

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,16 +1,23 @@
 import fastify, { FastifyInstance } from 'fastify'
 import fastifyCompress from 'fastify-compress'
+import fastifyCookie from 'fastify-cookie'
 import swaggerPlugin from './plugins/swagger'
 import rootRoute from './router'
-
+import authMiddleware from './middlewares/auth'
 export class Server {
   private _server: FastifyInstance
 
   constructor() {
     this._server = fastify({ logger: true })
 
+    const { COOKIE_SECRET } = process.env
+
     this._server.register(fastifyCompress)
+    this._server.register(fastifyCookie, {
+      secret: COOKIE_SECRET,
+    })
     this._server.register(swaggerPlugin)
+
     this._server.register(rootRoute, { prefix: '/' })
   }
 
