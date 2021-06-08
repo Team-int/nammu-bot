28acddd8195598e89c2c63ea03f07d9cf1a62c77
lukekeum <lukekeum11@gmail.com>
2021-06-09 02:13:55 +0900

    feat(icons): Add Embed Preview svg icon

--- packages/client/src/static/icons/svg/index.ts
+++ packages/client/src/static/icons/svg/index.ts
@@ -1,3 +1,4 @@
 export { default as BrandIcon } from './BrandIcon';
 export { default as BrandIconLarge } from './BrandIconLarge';
 export { default as ArrowDown } from './ArrowDown';
+export { default as EmbedPreview } from './EmbedPreview';
