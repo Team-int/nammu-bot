5b20b3756a4c3fc4b6490832af37064b57c1c862
lukekeum <lukekeum11@gmail.com>
2021-06-08 16:54:10 +0900

    feat(icons): Add arrow down icon

---
+++ packages/client/src/static/icons/svg/ArrowDown.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function ArrowDown() {
+  return (
+    <svg
+      width="56"
+      height="55"
+      viewBox="0 0 56 55"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.29 19.6854L28 30.1813L38.71 19.6854L42 22.9167L28 36.6667L14 22.9167L17.29 19.6854Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
--- packages/client/src/static/icons/svg/index.ts
+++ packages/client/src/static/icons/svg/index.ts
@@ -1,2 +1,3 @@
 export { default as BrandIcon } from './BrandIcon';
 export { default as BrandIconLarge } from './BrandIconLarge';
+export { default as ArrowDown } from './ArrowDown';
