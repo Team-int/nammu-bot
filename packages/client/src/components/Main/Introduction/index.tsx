b88102319c9316722ef79f7a066e0eab9aad478d
lukekeum <lukekeum11@gmail.com>
2021-06-08 16:55:50 +0900

    feat(introduction): Add window scroll action

--- packages/client/src/components/Main/Introduction/index.tsx
+++ packages/client/src/components/Main/Introduction/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import { css } from '@emotion/react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { css, keyframes } from '@emotion/react';
 import Icon from '../../../static/icons';
 
 export default function Introduction() {
+  const [height, setHeight] = useState<number>(0);
+
+  useEffect(() => {
+    setHeight(window.innerHeight);
+
+    function handleResize() {
+      setHeight(window.innerHeight);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div css={introductionStyleCSS}>
-      <div>
+      <div id="introduction__content">
         <div id="introduction__icon">
           <Icon name="BrandIconLarge" />
         </div>
@@ -15,19 +31,40 @@ export default function Introduction() {
           <span>간편하게 작성하세요</span>
         </div>
       </div>
+      <div id="introduction__check_down">
+        <span>더 알아보기</span>
+        <div onClick={() => window.scroll({ top: height, behavior: 'smooth' })}>
+          <Icon name="ArrowDown" />
+        </div>
+      </div>
     </div>
   );
 }
 
+const float = keyframes`
+  0% {
+    transform: translateY(0%)
+  }
+  50% {
+    transform: translateY(7.5%)
+  }
+  55% {
+    transform: translateY(7.5%)
+  }
+  100% {
+    transform: translateY(0%)
+  }
+`;
+
 const introductionStyleCSS = css`
-  top: 0;
   display: flex;
-  position: absolute;
-  align-items: center;
+  position: relative;
   justify-content: center;
-  height: 100vh;
+  align-items: center;
+  flex-direction: column;
+  height: calc(100vh - 155px);
   width: 100vw;
-  & > div {
+  & > #introduction__content {
     display: flex;
     width: 1000px;
     flex-direction: row;
@@ -47,6 +84,24 @@ const introductionStyleCSS = css`
           color: #ff8f8f;
         }
       }
+    }
+    z-index: auto;
+  }
+  & > #introduction__check_down {
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    bottom: 7px;
+    color: white;
+    font-weight: bold;
+    flex-direction: column;
+    span {
+      line-height: 0.3;
+    }
+    & > div {
+      cursor: pointer;
+      text-align: center;
+      animation: ${float} 1s ease infinite;
     }
   }
 `;
