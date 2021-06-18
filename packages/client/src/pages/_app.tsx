85cfa67831e158dcd171985932c0aecda5a00b44
lukekeum <lukekeum11@gmail.com>
2021-06-19 00:04:41 +0900

    feat(package): Add recoil package for state management

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import GlobalStyle from '../components/GlobalStyle';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </React.Fragment>
+    <RecoilRoot>
+      <React.Fragment>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </React.Fragment>
+    </RecoilRoot>
   );
 }
 
