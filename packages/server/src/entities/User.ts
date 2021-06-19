5545a8a820a9c01676edcaa28bc2d39a77411904
lukekeum <lukekeum11@gmail.com>
2021-06-19 22:31:57 +0900

    feat(entity): Add developer column for manage bot

--- packages/server/src/entities/User.ts
+++ packages/server/src/entities/User.ts
@@ -6,18 +6,21 @@ export class User extends BaseEntity {
   id: string
 
   @Index()
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ type: 'varchar', nullable: true })
   email: string
 
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ type: 'varchar', nullable: true })
   locale: string
 
-  @Column({ type: 'varchar', nullable: false, unique: true })
+  @Column({ type: 'varchar', nullable: true, unique: true })
   username: string
 
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ type: 'varchar', nullable: true })
   discriminator: string
 
   @Column({ type: 'boolean', default: false })
   verified: boolean
+
+  @Column({ type: 'boolean', nullable: true, default: false })
+  developer: boolean
 }
