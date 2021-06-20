1358e907ad57d4b0378c08bc3a43824412509d5a
lukekeum <lukekeum11@gmail.com>
2021-06-20 16:27:29 +0900

    feat(icon): Add Wheel svg icon

--- packages/client/src/static/icons/svg/index.ts
+++ packages/client/src/static/icons/svg/index.ts
@@ -2,3 +2,4 @@ export { default as BrandIcon } from './BrandIcon';
 export { default as BrandIconLarge } from './BrandIconLarge';
 export { default as ArrowDown } from './ArrowDown';
 export { default as EmbedPreview } from './EmbedPreview';
+export { default as Wheel } from './Wheel';
