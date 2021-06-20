8bd69495ca95853626f5f3ee6c43241265efcd43
lukekeum <lukekeum11@gmail.com>
2021-06-20 16:27:55 +0900

    feat($page/login): Add auth loading animation

--- packages/client/src/pages/auth/login.tsx
+++ packages/client/src/pages/auth/login.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
-import useUserAtomState, { userAtom } from '../../atom/User';
+import useUserAtomState from '../../atom/User';
 import client from '../../lib/client/server';
-import UserStorage from '../../lib/userStorage';
+import { css, keyframes } from '@emotion/react';
+import Icon from '../../static/icons/index';
 
 export default function Login() {
-  const [, setUserState] = useRecoilState(userAtom);
+  const [, setUserState] = useUserAtomState();
 
   const auth = async () => {
     try {
@@ -28,5 +28,54 @@ export default function Login() {
     });
   }, []);
 
-  return <></>;
+  return (
+    <div css={loginStyleCSS}>
+      <div>
+        <div id="icon">
+          <Icon name="Wheel" />
+        </div>
+        <h1>LOADING</h1>
+        <p>나무라는 이름은 그냥 밥먹다가 생각했어요</p>
+      </div>
+    </div>
+  );
 }
+
+const loginWheelAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+
+`;
+
+const loginStyleCSS = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  & > div {
+    display: flex;
+    flex-direction: column;
+    color: white;
+    align-items: center;
+    & > div[id='icon'] {
+      animation: ${loginWheelAnimation} infinite linear 1s;
+    }
+    & > h1 {
+      margin: 0;
+      padding: 0;
+      font-size: 2.25rem;
+      font-family: 'Roboto';
+      font-style: italic;
+      font-weight: 900;
+    }
+    & > p {
+      padding: 0;
+      margin: 1rem 0 0 0;
+    }
+  }
+`;
