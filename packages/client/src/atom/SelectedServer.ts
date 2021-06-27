3df72eb778f20353524131624d4db65bedba572b
lukekeum <lukekeum11@gmail.com>
2021-06-27 22:06:51 +0900

    feat(atom): Add SelectedServer atom state

---
+++ packages/client/src/atom/SelectedServer.ts
@@ -0,0 +1,25 @@
+import { atom, useRecoilState } from 'recoil';
+
+export interface Server {
+  id: string;
+  joined: boolean;
+  owner_id: string;
+  name: string;
+}
+
+export interface SelectedServer {
+  name?: string;
+  servers?: Server[];
+}
+
+export const selectedServerAtom = atom<SelectedServer>({
+  key: 'selected_server',
+  default: {
+    name: '',
+    servers: [],
+  },
+});
+
+export default function useSelectedServerAtomState() {
+  return useRecoilState(selectedServerAtom);
+}
