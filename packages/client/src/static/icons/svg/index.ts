c6059582ab733dd997f4c781b39cf1c7c7cc3bd2
lukekeum <lukekeum11@gmail.com>
2021-06-27 22:07:56 +0900

    feat(icons): Add CompleteArrowRight icon

--- packages/client/src/static/icons/svg/index.ts
+++ packages/client/src/static/icons/svg/index.ts
@@ -3,3 +3,4 @@ export { default as BrandIconLarge } from './BrandIconLarge';
 export { default as ArrowDown } from './ArrowDown';
 export { default as EmbedPreview } from './EmbedPreview';
 export { default as Wheel } from './Wheel';
+export { default as CompleteArrowRight } from './CompleteArrowRight';
