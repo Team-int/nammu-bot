2dad9a2bcca32cf45168453cb35a1e0f1d5fb96e
lukekeum <lukekeum11@gmail.com>
2021-07-04 19:29:58 +0900

    fix(middleware): Remove guild validation

--- packages/server/src/middlewares/guildChecker.ts
+++ packages/server/src/middlewares/guildChecker.ts
@@ -4,14 +4,9 @@ import fp from 'fastify-plugin'
 
 const guildCheckerMiddleware: FastifyPluginCallback = (fastify, opts, done) => {
   fastify.addHook('preHandler', async (req, res) => {
-    const id = req.headers['guild_id']
-
-    if (typeof id !== 'string')
-      return res.status(400).send({ message: 'Invalid guild id' })
+    const id = req.headers['guild_id'] as string
 
     const guild = await Guild.findOne({ id })
-
-    if (!guild) return res.status(404).send({ message: 'Guild not found' })
 
     req.guild = guild
   })
