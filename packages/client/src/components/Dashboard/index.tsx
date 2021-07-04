cad4d1971cbee6ac6eb73519f95487bb744bb950
lukekeum <lukekeum11@gmail.com>
2021-07-04 19:32:40 +0900

    fix(server): Change method of render component about server

--- packages/client/src/components/Dashboard/index.tsx
+++ packages/client/src/components/Dashboard/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { useEffect } from 'react';
 import useDashboardAtomState from '../../atom/Dashboard';
-import useSelectedServerAtomState from '../../atom/SelectedServer';
-import useGetUserGuilds from '../../hooks/useGetUserGuilds';
 import Loading from '../Loading';
 import ServerSelect from './ServerSelect';
+import DashboardMain from './DashboardMain';
+import useUserAtomState from '../../atom/User';
+import { useMemo } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-export default function Dashboard() {
-  const [dashboard, setDashboard] = useDashboardAtomState();
+export default React.memo(function Dashboard() {
+  const [dashboard] = useDashboardAtomState();
+  const [user] = useUserAtomState();
+  const logged_in = useMemo(() => user.logged_in, []);
+  const history = useRouter();
 
-  return (
-    <>
-      {(dashboard.loading && <Loading />) ||
-        (!dashboard.server && <ServerSelect />)}
-    </>
-  );
-}
+  const loading = useMemo(() => dashboard.loading, [dashboard.loading]);
+
+  useEffect(() => {
+    if (logged_in) return;
+    history.push('/');
+    return;
+  }, []);
+
+  useEffect(() => {
+    if (!dashboard.server) history.push('/servers');
+    if (dashboard.server) history.push(`/servers/${dashboard.server.id}`);
+    if (loading) history.push('/loading');
+  }, [dashboard, loading]);
+
+  return <></>;
+});
