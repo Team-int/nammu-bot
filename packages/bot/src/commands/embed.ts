8a80bbec036e448b38ce2007cb444a404171fda0
lukekeum <lukekeum11@gmail.com>
2021-07-18 20:24:12 +0900

    fix(command): Fix imports for controller and command

--- packages/bot/src/commands/embed.ts
+++ packages/bot/src/commands/embed.ts
@@ -1,11 +1,11 @@
 import EmbedController from '@src/controllers/embed/embed.controller'
-import Command, { Execute } from '@src/lib/commandManager'
+import Command, { Execute, TArguments } from '@src/lib/commandManager'
 import { Message } from 'discord.js'
 
 @Command(['임베드'])
 export default class Embed {
   @Execute
-  public async execute(message: Message, args: IArguments) {
+  public async execute(message: Message, args: TArguments) {
     const controller = new EmbedController()
     await controller.init(message, args)
   }
--- packages/bot/src/controllers/embed/embed.controller.ts
+++ packages/bot/src/controllers/embed/embed.controller.ts
@@ -1,12 +1,15 @@
 import { autoInjectable } from 'tsyringe'
 import { Message } from 'discord.js'
 import EmbedService from './embed.service'
+import { TArguments } from '@src/lib/commandManager'
 
 @autoInjectable()
 export default class EmbedController {
   constructor(private readonly service?: EmbedService) {}
 
-  async init(message: Message, args: IArguments) {
+  async init(message: Message, args: TArguments) {
+    if (!args) return false
+
     switch (args[0]) {
       case '생성':
         return await this.service?.create(message, args[1])
